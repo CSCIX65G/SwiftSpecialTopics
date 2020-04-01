ed66a13ab780ef014b43a307a5ca40ff7ca0a37f
Van Simmons <rvsrvs@gmail.com>
2020-04-01 08:36:36 -0400

    going to 0.8.4

--- Sources/Server/RelayController.swift
+++ Sources/Server/RelayController.swift
@@ -10,12 +10,44 @@ import SwiftyGPIO
 import Logging
 import MQTT
 
+func eventHandler(_ event: MQTT.Event<Void>) {
+    switch event {
+    case .clientStarted:
+        Server.logger.trace("Server started")
+    case .messageHandled(let resultArray):
+        resultArray.forEach { result in
+            switch result {
+            case .success:
+                Server.logger.trace("Message handled")
+            case .failure(let error):
+                errorHandler(error)
+            }
+        }
+    case .clientPaused:
+        Server.logger.trace("Server paused")
+    case .clientStopped:
+        Server.logger.trace("Server stopped")
+    case .clientError(let error):
+        errorHandler(error)
+    case .clientConnected:
+        Server.logger.trace("Server connected to MQTT")
+    case .clientReconnected:
+        Server.logger.trace("Server reconnected to MQTT")
+    case .clientConnecting(let host, let port):
+        Server.logger.trace("Server connecting @\(host):\(port)")
+    case .clientSubscribed(let topic):
+        Server.logger.trace("Server subscribed to: @\(topic)")
+    }
+}
+ 
 func errorHandler(_ swiftError: Swift.Error) {
     guard let error = swiftError as? MQTT.Error else {
         Server.logger.error("Received error: \(swiftError)")
         return
     }
     switch error {
+    case .couldNotStart:
+        Server.logger.error("MQTT client must run for at least 1 second")
     case .mosquittoError(let innerError):
         Server.logger.error("received \(innerError) from Mosquitto")
     case .throttled:
@@ -38,7 +70,7 @@ func errorHandler(_ swiftError: Swift.Error) {
 }
 
 struct RelayController {
-    static var resetRelay: MQTT.MessageProcessor {
+    static var resetRelay: MQTT.MessageProcessor<Void> {
         guard HostType.hostType == .rpi else {
             return MessageProcessor(matching: "relay") { _,_  in
                 .failure(MQTT.Error.unsupportedOperatingSystem)
--- Sources/Server/main.swift
+++ Sources/Server/main.swift
@@ -5,13 +5,13 @@ import MQTT
 let options = ServerOptions.parseOrExit()
 let logger = logging(for: options)
 
-logger.info("Server starting with log level: \(options.logLevel)")
+logger.info("RelayController starting with log level: \(options.logLevel)")
 
 let cancellable = MQTT.start(
     using: [RelayController.resetRelay],
     host: options.host,
-    errorHandler: errorHandler
+    eventHandler: eventHandler
 )
 
-try? cancellable.wait()
-logger.info("Server exited")
+try? cancellable.future.wait()
+logger.info("RelayController exited")
